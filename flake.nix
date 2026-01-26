67cad63014b39999ff07ff50137c7cc58864b917
benjaminkitt <1332211+benjaminkitt@users.noreply.github.com>
2026-01-26 06:07:03 +0000

    chore: update CLIProxyAPI to v6.7.24

--- flake.nix
+++ flake.nix
@@ -17,12 +17,12 @@
       ];
 
       # Version and hash information (updated by GitHub Action)
-      version = "6.6.109";
+      version = "6.7.24";
       hashes = {
-        "x86_64-linux" = "sha256-BnmwGOfJklw0Z76u5+AqrjO8rhHKSq3XnEWIRnS03IY=";
-        "aarch64-linux" = "sha256-21ukewFCjjsy8hqNL2tOD4HuDhubNgPDQ0LJ+1ZKBkI=";
-        "x86_64-darwin" = "sha256-dtsj87fq2QUBm96xcy7rh+Q0oKoMEwjPPleEq51t8+Y=";
-        "aarch64-darwin" = "sha256-BpxnL3keMLY8DR7Lazq+rjAZKme1JjcOUz/KJ7TptNo=";
+        "x86_64-linux" = "sha256-7JfQ/bx2GW0y9Yk2xAQHebILjFbTr2rPz1n5sbVbai0=";
+        "aarch64-linux" = "sha256-ZAT6+KHsIxfVs1NdT1a2M7FI3TQEzSTBfV2BJSbiXiQ=";
+        "x86_64-darwin" = "sha256-e//N7xfNwR4Pf4xYc3kAO8F1wz2dz/8T3G+xVGdtfAg=";
+        "aarch64-darwin" = "sha256-FmDqFhlSclIUkr//hZ2LKXFIbbXpTSct1SgJRzzS2dQ=";
       };
 
       # Map Nix system to release asset naming
